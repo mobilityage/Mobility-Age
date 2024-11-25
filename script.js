9b2a22200f5a5b7d16ec5b8ac3254f3ed776b7ba
mobilityage <joshume@hotmail.com>
2024-11-25 12:50:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { config } from './config.js';
 let assessmentData = {
     userAge: null,
     currentPose: 0,
@@ -101,7 +102,7 @@ const response = await fetch('https://api.openai.com/v1/chat/completions', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
+        'Authorization': `Bearer ${config.apiKey}`
     },
     body: JSON.stringify(requestBody)
 });
