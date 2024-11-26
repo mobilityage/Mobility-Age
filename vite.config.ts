229dbc9623e65863f528dbe04fd24de00768473b
mobilityage <joshume@hotmail.com>
2024-11-26 12:14:44 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@types': path.resolve(__dirname, './src/types')
     }
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      external: [],
-      output: {
-        manualChunks: undefined
-      }
-    }
+    sourcemap: true
   }
 })
